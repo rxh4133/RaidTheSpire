b40fd40043044ec94e27d1d89502f562b5872ebb
Ryan Hartzell <rxh4133@rit.edu>
2019-02-03 20:45:53 -0500

    Fixed the bat compile and run

--- CompileAndRun.bat
+++ CompileAndRun.bat
@@ -1,4 +1,4 @@
 color 02
-javac src/client/gui/*.java src/client/*.java src/global/*.java src/global/cards/*.java src/global/cards/retributor/*.java src/global/relics/*.java src/global/statuseffects/*.java src/server/*.java src/server/managers/*.java
+javac client/gui/*.java client/*.java global/*.java global/cards/*.java global/cards/retributor/*.java global/relics/*.java global/statuseffects/*.java server/*.java server/managers/*.java
 java src/client/Runner
 pause
