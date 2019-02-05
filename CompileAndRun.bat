3a945f425b4194a71fe2581a21d42f579473b553
Ryan Hartzell <rxh4133@rit.edu>
2019-02-04 19:26:33 -0500

    Forgot to add the enemies folder to the compile and run batch file, whoops.

--- CompileAndRun.bat
+++ CompileAndRun.bat
@@ -1,5 +1,5 @@
 color 02
 cd src
-javac client/gui/*.java client/*.java global/*.java global/cards/*.java global/cards/retributor/*.java global/relics/*.java global/statuseffects/*.java global/enemyactions/*.java server/*.java server/managers/*.java
+javac client/gui/*.java client/*.java global/*.java global/cards/*.java global/cards/retributor/*.java global/relics/*.java global/statuseffects/*.java global/enemyactions/*.java global/enemies/*.java server/*.java server/managers/*.java
 java client/Runner
 pause
